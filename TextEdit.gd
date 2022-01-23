862070187bdbbd02f4607cbc0d0b852e8d32c4ee
jr <jrrdmb@gmail.com>
2022-01-23 12:56:59 -0600

    use _input(e) rather than _process(d)

--- TextEdit.gd
+++ TextEdit.gd
@@ -4,8 +4,9 @@ extends TextEdit
 onready var font_resource = get('custom_fonts/font')
 
 
-func _process(delta):
-	if Input.is_action_just_pressed("zoom_in"):
-		font_resource.set_size(font_resource.get_size() + 1)
-	if Input.is_action_just_pressed("zoom_out"):
-		font_resource.set_size(font_resource.get_size() - 1)
+func _input(event):
+	if event is InputEventWithModifiers:
+		if Input.is_action_just_pressed("zoom_in"):
+			font_resource.set_size(font_resource.get_size() + 1)
+		elif Input.is_action_just_pressed("zoom_out"):
+			font_resource.set_size(font_resource.get_size() - 1)
