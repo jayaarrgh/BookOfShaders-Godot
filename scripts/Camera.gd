c348750e83389738168abed6e2c1db476e82fab4
jr <jrrdmb@gmail.com>
2022-01-24 12:15:08 -0600

    handle zoom in _unhandled_input

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,24 +1,35 @@
 extends Spatial
 
 
-var mouse_sens = 0.2
-var middle_clicked = false
-var shift_pressed
+const MOUSE_SENS = 0.2
+const ZOOM_STEP  = 0.2
+const MOVE_MOD   = 0.05
 
 onready var gimbleX = $GimbleX
 onready var camera = $GimbleX/Camera
 
-func _ready():
-	pass # Replace with function body.
+var middle_clicked = false
+var shift_pressed = false
+
+
+# use unhandled input for zoom, so scrolling in menus does not effect 3d view	
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		# need to be ignored or captured by UI first
+		if event.button_index == BUTTON_WHEEL_UP:
+			camera.translate_object_local(Vector3(0.0, 0.0, -ZOOM_STEP))
+		if event.button_index == BUTTON_WHEEL_DOWN:
+			camera.translate_object_local(Vector3(0.0, 0.0, ZOOM_STEP))
+
 
 func _input(event):
-	# TODO: other in editor mouse controls for camera
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_MIDDLE:
 			if event.pressed:
 				middle_clicked = true
 			elif !event.pressed: 
 				middle_clicked = false
+	
 	if event is InputEventKey:
 		if event.scancode == KEY_SHIFT:
 			 shift_pressed = event.is_pressed()
@@ -27,8 +38,8 @@ func _input(event):
 		if middle_clicked:
 			if shift_pressed:
 				# TODO: limit camera movement to keep object in frame
-				camera.translate_object_local(Vector3(event.relative.x*mouse_sens*0.05, event.relative.y*mouse_sens*0.05, 0.0))
+				camera.translate_object_local(Vector3(-event.relative.x*MOUSE_SENS*MOVE_MOD, event.relative.y*MOUSE_SENS*MOVE_MOD, 0.0))
 			else:
-				self.rotate_y(deg2rad(-event.relative.x*mouse_sens))
-				gimbleX.rotate_x(deg2rad(-event.relative.y*mouse_sens))
+				gimbleX.rotate_x(deg2rad(-event.relative.y*MOUSE_SENS))
+				self.rotate_y(deg2rad(-event.relative.x*MOUSE_SENS))
 			
